3f74435a4ffd83875fa74363fad50be93113443f
Nicolas Osorio <nicolasosorio@192.168.1.10>
2025-09-07 11:56:40 -0500

    added checkbox to build hist

--- app.py
+++ app.py
@@ -14,3 +14,28 @@ if hist_button: # al hacer clic en el botón
      
          # mostrar un gráfico Plotly interactivo
          st.plotly_chart(fig, use_container_width=True)
+
+scatter_button = st.button('Construir grafico de dispersion') # crear un botón para grafico de dispersion
+     
+if scatter_button: # al hacer clic en el botón
+         # escribir un mensaje
+         st.write('Creación de un grafico de dispersion para el conjunto de datos de anuncios de venta de coches')
+         
+         # crear un grafico de dispersion
+         fig = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
+     
+         # mostrar un gráfico Plotly interactivo de dispersion
+         st.plotly_chart(fig, use_container_width=True)
+
+# crear una casilla de verificación
+build_histogram = st.checkbox('Construir un histograma')
+
+if build_histogram: # si la casilla de verificación está seleccionada
+         # escribir un mensaje
+         st.write('Construir un histograma para la columna odómetro')
+
+         # crear un histograma
+         fig = px.histogram(car_data, x="odometer")
+     
+         # mostrar un gráfico Plotly interactivo
+         st.plotly_chart(fig, use_container_width=True)
